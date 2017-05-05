e16f8e217ca48a0be7e0225ff238f03694bc3167
HaslinD <haslind@unitec.edu>
2017-05-05 15:16:27 -0600

    Tercer Commit

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <sstresm>
 
 using namespace std;
+
+int menu(int);
+int convert(int);
 
 int main(){
     bool salir = false;
     while (!salir){
         switch(menu()){
             case 1:{
-                
+                int numeroN, count = 0;
+		stringstream stringN1, stringN2;
+		String binary;
+
+		cout << "Ingrese Un Numero: ";
+		cin >> numeroN;
+		for(int i = 0, i < numeroN, i++){
+			stringN1 << convert(i);
+			binary = stringN1.str();
+			
+			for(int j = 0, j < binary.size(), j++){
+				if(binary[j] == "1"){
+					count++;	
+				}
+			}
+			if(count % 2 == 0){
+				stringN2 << i << ",";		
+			}
+		}
+		
                 break;}
 
             case 2:{
@@ -21,6 +45,11 @@ int main(){
     }//Fin while
 
     return 0;
+}
+
+stringstream convert(int value){
+    
+
 }
 
 int menu(){
